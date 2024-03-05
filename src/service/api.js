d8c05dc0579e88ebca1a4b0e18fce07eeda07d57
eduarda <andradeeduarda1998@gmail.com>
2024-03-05 20:57:54 -0300

    Atualização do site do render

--- src/service/api.js
+++ src/service/api.js
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   
-baseURL: "https://food-teste.onrender.com"
+//baseURL: "https://food-teste.onrender.com"
+
+ baseURL: "https://food-backend-4olx.onrender.com"
 });
